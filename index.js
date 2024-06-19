e5699891ebe7ed4994d3674f62e972e6398dcbd6
hanumantjain <hanumantjain939@gmail.com>
2024-06-20 00:52:50 +0530

    url

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const urlRoute = require('./routes/url')
+const { connectMongoDB } = require('./connections/connect')
+
+const app = express()
+const PORT = 8001
+
+connectMongoDB('mongodb://127.0.0.1:27017/short-url')
+    .then( ()=> console.log('MongoDB connected'))
+
+app.use(express.json())
+
+app.use('/url', urlRoute)
+
+app.listen(PORT, ()=> console.log(`Server started at port : ${PORT}`))
